9d8efd765182e5131cdaadd12a3767d2f4f7d322
Diogenes Brito <d+code@uxdiogenes.com>
2013-06-02 14:17:11 -0400

    This should fix the getQuarter logic?
    I guess that's why people write tests, hah. That's on the todo list.

--- index.js
+++ index.js
@@ -72,8 +72,16 @@ quarters.getQuarterEnd = function (quarter, year) {
 **/
 quarters.getQuarter = function (date) {
   date = date || new Date(); // If no date supplied, use today
-  var quarters = [4, 1, 2, 3];
-  return quarters[Math.floor(date.getMonth() / 3)];
+  var month = date.getMonth() + 1;
+  if (month >= 1 && month <= 3) {
+    return 1;
+  } else if (month >= 4 && month <= 6) {
+    return 2;
+  } else if (month >= 7 && month <= 9) {
+    return 3;
+  } else {
+    return 4;
+  }
 };
 
 module.exports = exports = quarters;
